2206aaf1a83910f6ac5c9a0ad815454ffb1f3fe6
Yumei <yumei@YMacBook-Pro.local>
2021-04-01 15:43:38 -0400

    local tweak

--- autos/urls.py
+++ autos/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='all'),
-    path('autos', autos.as_view, name='autos'),
-    # path('autolist/', views.auto_list, name='auto_list'),
-    # path('auto/<int:id>/', views.post_detail, name='auto_detail'),
-    # path('auto/<int:id>/update', views.post_detail, name='auto_update'),
-    # path('auto/create/', views.post_create, name='auto_create'),
-
+    path('autos/', views.Autos.as_view(), name='autos'),
+    path('autolist/', views.AutoList.as_view(), name='auto_list'),
+    path('autocreate/', views.AutoCreate.as_view(), name='auto_create'),
+    # path('create/', views.AutoCreate.as_view(), name='auto_create'),
+    path('auto/<int:id>/', views.AutoDetail.as_view, name='auto_detail'),
+    # path('auto/<int:id>/update', views.auto_detail, name='auto_update'),
+    
 ]
--- autos/views.py
+++ autos/views.py
@@ -9,8 +9,8 @@ from autos.models import Auto, Make
 # from autos.forms import MakeForm
 
 # Create your views here.
-
-class MainView(LoginRequiredMixin, View):
+# class MainView(LoginRequiredMixin, View):
+class MainView(View):
     def get(self, request):
         mc = Make.objects.all().count()
         al = Auto.objects.all()
@@ -19,17 +19,18 @@ class MainView(LoginRequiredMixin, View):
         return render(request, 'autos/auto_list.html', ctx)
 
 
-class MakeView(LoginRequiredMixin, View):
-    def get(self, request):
-        ml = Make.objects.all()
-        ctx = {'make_list': ml}
-        return render(request, 'autos/make_list.html', ctx)
+class MakeView(View):
+# class MakeView(LoginRequiredMixin, View):
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('autos:all')
 
 
 # We use reverse_lazy() because we are in "constructor attribute" code
 # that is run before urls.py is completely loaded
 
-class MakeCreate(LoginRequiredMixin, CreateView):
+class MakeCreate(CreateView):
+# class MakeCreate(LoginRequiredMixin, CreateView):
     model = Make
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
@@ -39,13 +40,14 @@ class MakeCreate(LoginRequiredMixin, CreateView):
 # AutoUpdate (below) is doing the same thing with no code
 # and no form by extending UpdateView
 
-class MakeUpdate(LoginRequiredMixin, UpdateView):
+class MakeUpdate(UpdateView):
+# class MakeUpdate(LoginRequiredMixin, UpdateView):
     model = Make
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
 
-
-class MakeDelete(LoginRequiredMixin, DeleteView):
+class MakeDelete(DeleteView):
+# class MakeDelete(LoginRequiredMixin, DeleteView):
     model = Make
     fields = '__all__'
     success_url = reverse_lazy('autos:all')
@@ -56,10 +58,12 @@ class MakeDelete(LoginRequiredMixin, DeleteView):
 # Build a form object dynamically based on the fields
 # value in the constructor attributes
 
-class Autos(TemplateView):
+# class Autos(View):
+class Autos( LoginRequiredMixin, ListView):
     model = Auto
     fields = '__all__'
     success_url = reverse_lazy('autos')
+    # success_url = reverse_lazy('autos/auto_list')
 # class Autos(LoginRequiredMixin, ListView):
 #     def get(self, request):
 #         mc = Make.objects.all().count()
@@ -68,24 +72,31 @@ class Autos(TemplateView):
 #         ctx = {'make_count': mc, 'auto_list': al}
 #         return render(request, 'autos/auto_list.html', ctx)
 
-class AutoList(LoginRequiredMixin, ListView):
-    pass
+class AutoList(ListView):
+# class AutoList(LoginRequiredMixin, ListView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autolist')
 
+# class AutoCreate(CreateView):
 class AutoCreate(LoginRequiredMixin, CreateView):
+    # pass
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('autocreate')
+
+class AutoDetail(DetailView):
+    model = Auto
+
+class AutoUpdate(UpdateView):
+# class AutoUpdate(LoginRequiredMixin, UpdateView):
     pass
     # model = Auto
     # fields = '__all__'
     # success_url = reverse_lazy('autos:all')
 
-
-class AutoUpdate(LoginRequiredMixin, UpdateView):
-    pass
-    # model = Auto
-    # fields = '__all__'
-    # success_url = reverse_lazy('autos:all')
-
-
-class AutoDelete(LoginRequiredMixin, DeleteView):
+class AutoDelete(DeleteView):
+# class AutoDelete(LoginRequiredMixin, DeleteView):
     pass
     # model = Auto
     # fields = '__all__'
