0ffa3f7a0d79de854c9c57ac272c288634734eba
Yumei <yumei@YMacBook-Pro.local>
2021-04-12 11:06:07 -0400

    model

--- ads/models.py
+++ ads/models.py
@@ -14,7 +14,8 @@ class Ad(models.Model) :
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.title 
+        
 
     # Picture
     picture = models.BinaryField(null=True, editable=True)
