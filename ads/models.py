1cb043cb202e69f57df8a1b8216d4fa98a5f6e61
Yumei <yumei@YMacBook-Pro.local>
2021-04-12 11:19:04 -0400

    django 3.1.8

--- ads/models.py
+++ ads/models.py
@@ -13,6 +13,10 @@ class Ad(models.Model) :
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+
     # Picture
     picture = models.BinaryField(null=True, editable=True)
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
