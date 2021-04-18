bc189e03ffde56b22a6485df5b7860994f05fd18
Yumei <yumei@YMacBook-Pro.local>
2021-04-18 12:34:20 -0400

    tweak fav

--- ads/views.py
+++ ads/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 from django.urls import reverse_lazy, reverse
-from .models import Ad, Comment
+from .models import Ad, Comment, Fav
 from ads.forms import AdCreateForm, CommentForm
 from pics.models import Pic
 from pics.views import PicCreateView, PicDetailView, PicDeleteView
@@ -18,6 +18,18 @@ class AdListView(ListView):
     model = Ad
     # fields = ['title', 'price', 'text']
     # success_url = reverse_lazy('ads:ad_list')
+    template_name = "ads/ad_list.html"
+
+    def get(self, request) :
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            # favorites = [2, 4, ...] using list comprehension
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'ad_list' : ad_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
 
 class AdCreateView(LoginRequiredMixin, View):
     template_name = 'ads/ad_form.html'
@@ -116,6 +128,41 @@ def stream_file(request, pk):
     response['Content-Length'] = len(ad.picture)
     response.write(ad.picture)
     return response
+
+
+# csrf exemption in class based views
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, ad=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Thing, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, thing=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
+
+
+
+
 
 # class PicListView(OwnerListView):
 #     model = Pic
