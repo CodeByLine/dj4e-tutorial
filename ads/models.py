9a1d1b1acc51ac0c7c9edc26aff4821720112d0a
Yumei <yumei@YMacBook-Pro.local>
2021-04-19 23:52:24 -0400

    wrap up fav

--- ads/models.py
+++ ads/models.py
@@ -34,6 +34,7 @@ class Comment(models.Model) :
 
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='ad_owner')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
